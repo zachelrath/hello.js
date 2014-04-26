24cc559639f09c3b2b88345bd33b7ca7458e7909
Andrew Dodson <andrew.j.dodson@gmail.com>
2014-04-26 22:35:00 +1000

    Update utils/realpath, include spec

--- src/utils/realPath.js
+++ src/utils/realPath.js
@@ -3,19 +3,25 @@
 // Converts relative URL's to fully qualified URL's
 define(function(){
 
+	var location = window.location;
+	var regAsc = /\/[^\/]+\/\.\.\//g;
+
 	return function(path){
 
-		var location = window.location;
-
-		if( path.indexOf('/') === 0 ){
-			path = location.protocol + '//' + location.host + path;
+		if(!path){
+			return location.href;
+		}
+		else if( path.indexOf('/') === 0 ){
+			path = location.protocol + ( path.indexOf('//') === 0 ? path : '//' + location.host + path );
 		}
 		// Is the redirect_uri relative?
 		else if( !path.match(/^https?\:\/\//) ){
 			path = (location.href.replace(/#.*/,'').replace(/\/[^\/]+$/,'/') + path).replace(/\/\.\//g,'/');
 		}
-		while( /\/[^\/]+\/\.\.\//g.test(path) ){
-			path = path.replace(/\/[^\/]+\/\.\.\//g, '/');
+
+		while( regAsc.test(path) ){
+			regAsc.lastIndex = 0;
+			path = path.replace(regAsc, '/');
 		}
 		return path;
 	};
